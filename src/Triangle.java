5edfb43bfe1d0305365b5a99594c7df59afec8ca
melmanss <74724829+melmanss@users.noreply.github.com>
2025-01-31 13:27:01 +0200

    Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,12 +1,10 @@
 public class Triangle implements Shape {
     private final double base;
     private final double height;
-
     public Triangle(double base, double height) {
         this.base = base;
         this.height = height;
     }
-
     @Override
     public double area() {
         return 0.5 * base * height; /*Площа трикутника*/
