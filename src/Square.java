d3a8f30649f56d73311a67c27ef71b94a172a3e6
melmanss <74724829+melmanss@users.noreply.github.com>
2025-01-31 13:26:39 +0200

    Square.java

--- src/Square.java
+++ src/Square.java
@@ -1,10 +1,8 @@
 public class Square implements Shape {
     private final double side;
-
     public Square(double side) {
         this.side = side;
     }
-
     @Override
     public double area() {
         return side * side; /*Площа квадрата*/
