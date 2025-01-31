13ead67f1f068cbcb7be61d65868f7344f39d0b9
melmanss <74724829+melmanss@users.noreply.github.com>
2025-01-31 13:27:27 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
                 new Triangle(4, 3),
                 new Square(2)
         };
-
         double totalArea = ShapeAreaCalculator.totalArea(shapes);
         System.out.println("Сумарна площа всіх фігур: " + totalArea);
     }
